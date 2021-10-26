146c0fd000eaa7029685c0d13180792d8d0dc179
Sijan Malla <44745915+season101@users.noreply.github.com>
2021-10-26 02:36:16 -0500

    Add files via upload

---
+++ JDBCReporting/src/main/java/com/example/CSCI4044Prog1/JDBCReporting/JdbcReportingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSCI4044Prog1.JDBCReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcReportingApplication.class, args);
+	}
+
+}
---
+++ JDBCReporting/src/test/java/com/example/CSCI4044Prog1/JDBCReporting/JdbcReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSCI4044Prog1.JDBCReporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
